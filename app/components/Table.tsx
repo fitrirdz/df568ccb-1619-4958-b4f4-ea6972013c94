f66b0e36c99ed13ea0b0fcbca7564d8e094fc7aa
Fitri R Dewi <fitri@softwareseni.com>
2024-09-03 16:04:05 +0700

    Initial view

---
+++ app/components/Table.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import ArrowIcon from '@/public/icons/up-arrow.png';
+import Image from 'next/image';
+
+const Table = () => {
+  const dummyData = [
+    {
+      firstName: 'Jack',
+      lastName: 'Hansen',
+      position: 'CEO',
+      phone: '(818) 555-2387',
+      email: 'robertr@dx-email.my',
+    },
+    {
+      firstName: 'Emily',
+      lastName: 'Smith',
+      position: 'COO',
+      phone: '(312) 555-0198',
+      email: 'emilys@dx-email.my',
+    },
+    {
+      firstName: 'Michael',
+      lastName: 'Johnson',
+      position: 'CFO',
+      phone: '(213) 555-0187',
+      email: 'michaelj@dx-email.my',
+    },
+    {
+      firstName: 'Jessica',
+      lastName: 'Williams',
+      position: 'CTO',
+      phone: '(646) 555-0123',
+      email: 'jessicaw@dx-email.my',
+    },
+    {
+      firstName: 'David',
+      lastName: 'Brown',
+      position: 'CMO',
+      phone: '(415) 555-0987',
+      email: 'davidb@dx-email.my',
+    },
+    {
+      firstName: 'Sarah',
+      lastName: 'Davis',
+      position: 'VP of Sales',
+      phone: '(202) 555-0111',
+      email: 'sarahd@dx-email.my',
+    },
+    {
+      firstName: 'Matthew',
+      lastName: 'Martinez',
+      position: 'VP of Marketing',
+      phone: '(305) 555-0176',
+      email: 'matthewm@dx-email.my',
+    },
+    {
+      firstName: 'Ashley',
+      lastName: 'Garcia',
+      position: 'VP of Product',
+      phone: '(210) 555-0209',
+      email: 'ashleyg@dx-email.my',
+    },
+    {
+      firstName: 'Brian',
+      lastName: 'Wilson',
+      position: 'VP of Engineering',
+      phone: '(602) 555-0154',
+      email: 'brianw@dx-email.my',
+    },
+    {
+      firstName: 'Rebecca',
+      lastName: 'Lopez',
+      position: 'HR Manager',
+      phone: '(704) 555-0192',
+      email: 'rebeccal@dx-email.my',
+    },
+    {
+      firstName: 'Daniel',
+      lastName: 'Harris',
+      position: 'Legal Counsel',
+      phone: '(917) 555-0189',
+      email: 'danielh@dx-email.my',
+    },
+  ];
+
+  return (
+    <table className='flex-1 w-full border border-gray-300 border-b-0'>
+      <thead>
+        <tr className='border-b border-gray-300 text-gray-500 font-semibold'>
+          <td className='p-4 hover:bg-gray-100 hover:cursor-pointer hover:text-black flex items-center gap-2'>
+            First Name
+            <Image
+              src={ArrowIcon}
+              alt='arrow'
+              width={16}
+              height={24}
+              style={{ filter: 'grayscale(100%)' }}
+            />
+          </td>
+          <td className='p-4 hover:bg-gray-100 hover:cursor-pointer hover:text-black'>
+            Last Name
+          </td>
+          <td className='p-4 hover:bg-gray-100 hover:cursor-pointer hover:text-black'>
+            Position
+          </td>
+          <td className='p-4 hover:bg-gray-100 hover:cursor-pointer hover:text-black'>
+            Phone
+          </td>
+          <td className='p-4 hover:bg-gray-100 hover:cursor-pointer hover:text-black'>
+            Email
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        {dummyData?.map((item, index) => (
+          <tr key={index} className='border-b border-gray-300 text-gray-700'>
+            <td className='p-4'>{item.firstName}</td>
+            <td className='p-4'>{item.lastName}</td>
+            <td className='p-4'>{item.position}</td>
+            <td className='p-4'>{item.phone}</td>
+            <td className='p-4'>{item.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
